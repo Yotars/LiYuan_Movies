1f6b655646702c860fc2d427078eafeee82d72e6
Yotars <yot417@126.com>
2024-01-04 21:04:10 +0800

    创建项目

---
+++ Backend/LiYuanMovies/src/main/java/com/liyuanmovies/LiYuanMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.liyuanmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiYuanMoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiYuanMoviesApplication.class, args);
+    }
+
+}
---
+++ Backend/LiYuanMovies/src/test/java/com/liyuanmovies/LiYuanMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liyuanmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiYuanMoviesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
