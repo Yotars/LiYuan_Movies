a054dd44d65d78e971a81acb8ac59d3b436d5ac2
Yotars <yot417@126.com>
2024-01-29 10:18:54 +0800

    设置默认路由

--- Frontend/LiYuanMovies/src/App.vue
+++ Frontend/LiYuanMovies/src/App.vue
@@ -1,12 +1,10 @@
 <template>
   <div>
-    <UploadComponent />
     <RouterView />
   </div>
 </template>
 
 <script setup lang="ts">
-import UploadComponent from './components/UploadComponent.vue';
 </script>
 
 <style scoped></style>
