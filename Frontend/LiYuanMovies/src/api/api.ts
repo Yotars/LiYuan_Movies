58f5db4040c7e573b478679b5daf8a34410ca0f3
Yotars <yot417@126.com>
2024-01-29 12:52:37 +0800

    封装动态生成请求函数

---
+++ Frontend/LiYuanMovies/src/api/api.ts
@@ -0,0 +1,101 @@
+import axios from "axios";
+import url from "../api/url";
+
+const baseURL: string = "/api";
+
+interface urls {
+  [key: string]:
+    | {
+        api?: string[];
+        not?: string[];
+      }
+    | string;
+}
+interface apis {
+  [key: string]: {
+    [key: string]: any;
+  };
+}
+
+const urls: urls = url;
+
+/**
+ * 根据读取到的配置生成请求函数
+ * @param key 对应配置的 key
+ * @returns 生成的请求函数
+ */
+const requeset = (key: string) => {
+  /**
+   * 读取配置
+   */
+  const url = urls[key];
+
+  /**
+   * 请求配置
+   * @param url 请求路径
+   * @param item 请求体
+   * @param method 请求方法
+   */
+  const config = (
+    url: string,
+    item: { [key: string]: string },
+    method: string = "POST"
+  ) => {
+    return {
+      url: url,
+      method: method,
+      params: item,
+      baseURL: baseURL,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+  };
+
+  /**
+   * 默认的 apis
+   */
+  const method = ["GET", "POST", "PUT", "DELETE"];
+  const api = ["list", "insert", "change", "delete"];
+
+  /**
+   * 如果配置中有 not 则删除 apis 中对应的 api
+   * 如果配置中有 api 则 apis 中添加对应的 api
+   */
+  if (typeof url === "object" && typeof url.not !== "undefined") {
+    const index = [];
+    for (let i = 0; i < url.not.length; i++) {
+      index.push(api.indexOf(url.not[i]));
+    }
+    for (let i = 0; i < index.length; i++) {
+      api.splice(index[i], 1);
+      method.splice(index[i], 1);
+    }
+  } else if (typeof url === "object" && typeof url.api !== "undefined") {
+    api.push(...url.api);
+  }
+
+  /**
+   * 根据最后生成的 api 生成请求
+   */
+  const apis: apis = {};
+  for (let i = 0; i < api.length; i++) {
+    let url = "/" + key;
+    if (method[i] === undefined) {
+      url = url + "/" + api[i];
+    }
+    apis[api[i]] = async (item: { [key: string]: string }) => {
+      const res = await axios.request(config(url, item, method[i]));
+      if (res.status === 200) return res.data;
+      else "error";
+    };
+  }
+  return apis;
+};
+
+const apis: apis = {};
+for (const key in urls) {
+  apis[key] = requeset(key);
+}
+
+export default apis;
---
+++ Frontend/LiYuanMovies/src/api/url.ts
@@ -0,0 +1,43 @@
+/**
+ * 定义url对象中的参数
+ */
+interface urls {
+  [key: string]:
+    | {
+        api?: string[];
+        not?: string[];
+      }
+    | string;
+}
+/**
+ *
+ * 定义api接口的url
+ * api 根据 url 生成对应的 接口 如：GET、POST、PUT、DELETE请求等等。
+ *
+ * 输入格式：
+ * 
+ * '接口名': {
+ *   api: ['自定义接口名'],   // 可选参数，作用：将一些自定义的接口生成到 api 中
+ *   not: ['默认接口名']      // 可选参数，作用：过滤默认生成的请求，如：GET、POST、PUT、DELETE
+ * },
+ * 
+ * 或者：
+ * 
+ * '接口名':'接口名'          // 默认生成 GET、POST、PUT、DELETE请求。
+ */
+const url: urls = {
+  user: {
+    api: ["login", "isAdmin"],
+  },
+  info: {
+    not: ["insert"],
+  },
+  oder: {
+    api: ["cancel", "done"],
+  },
+  movies: "movies",
+  room: "room",
+  seat: "seat",
+};
+
+export default url;
