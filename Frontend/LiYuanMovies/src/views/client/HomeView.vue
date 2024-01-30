21ec602f77510183c0bcabd34f67dd5a46acb4cf
Yotars <yot417@126.com>
2024-01-30 15:22:25 +0800

    创建Home、Console组件

---
+++ Frontend/LiYuanMovies/src/views/client/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>home</div>
+  {{ list }}
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const list = ref({});
+</script>
+
+<style scoped></style>
---
+++ Frontend/LiYuanMovies/src/views/console/ConsoleView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <n-layout :inverted="inverted" content-style="height: 100vh;">
+      <n-layout-header bordered>颐和园路</n-layout-header>
+      <n-layout has-sider>
+        <n-layout-sider bordered content-style="padding: 24px;">
+          <div style="height: 100%;">123</div>
+        </n-layout-sider>
+        <n-layout-content bordered content-style="padding: 24px;">
+          平山道
+        </n-layout-content>
+      </n-layout>
+      <n-layout-footer>成府路</n-layout-footer>
+    </n-layout>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const inverted = ref(true);
+</script>
+
+<style scoped></style>
