94a74895546576a9bdb751641faabc8f80ce8499
Yotars <yot417@126.com>
2024-01-30 15:19:09 +0800

    优化入口文件，添加公共样式

--- Frontend/LiYuanMovies/src/main.ts
+++ Frontend/LiYuanMovies/src/main.ts
@@ -1,12 +1,15 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
 
-const app = createApp(App)
+import { router } from "./router";
+import NaiveUI from "naive-ui";
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+app.use(NaiveUI);
+
+app.mount("#app");
