5d465fff3b93e0c6664d3208c4a05b48097cfec1
Yotars <yot417@126.com>
2024-02-02 17:57:15 +0800

    添加控制面板判断

--- Frontend/LiYuanMovies/src/App.vue
+++ Frontend/LiYuanMovies/src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>
+  <div v-if="isConsole">
+    <console />
+  </div>
+  <div v-else>
     <RouterView />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import console from "./views/console/ConsoleView.vue";
+const link = window.location.href.split("/")[3];
+const isConsole = link === "console";
+</script>
 
-<style scoped>
-@import "./style/common.css";
-</style>
+<style scoped></style>
