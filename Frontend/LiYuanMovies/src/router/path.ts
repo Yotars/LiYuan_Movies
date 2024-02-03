27cd558cec4ff8bc03021ab58dc74ed8ca0f06a1
Yotars <yot417@126.com>
2024-02-03 18:36:29 +0800

    router-path: 添加可选参数 => redirect 默认重定向

--- Frontend/LiYuanMovies/src/router/path.ts
+++ Frontend/LiYuanMovies/src/router/path.ts
@@ -2,27 +2,34 @@ interface path {
   [key: string]: {
     name?: string;
     path?: string;
+    redirect?: string;
   };
 }
 /**
  * 这里添加自定义路由 name 与 path，例：
  * [组件路径: string]: {
- *   name: '组件名',
- *   path: '路径名' //  没有path参数，默认为 '/' + 组件名
+ *   name: '组件名' //  没有 name 参数，默认从组件路径中解析
+ *   path: '路径名' //  没有 path 参数，默认为 '/' + 组件名
+ *   redirect: '默认显示页面' //  没有 redirect 参数，默认为 '/'
  * }
  * 支持添加自定义组件，但路径一定要写对
  */
 export const path: path = {
-  "../views/console/ConsoleView.vue": {
-    name: "console",
-    path: "/",
-  },
-  "../views/console/HomeView.vue": {
-    name: "consoleHome",
-    path: "/home",
-  },
   "../views/client/HomeView.vue": {
     name: "home",
     path: "/",
+  },
+  "../views/console/ConsoleView.vue": {
+    name: "console",
+    path: "/",
+    redirect: "/home",
+  },
+  "../views/console/HomeConsole.vue": {
+    name: "consoleHome",
+    path: "/home",
+  },
+  "../views/console/MovieConsole.vue": {
+    name: "consoleMovie",
+    path: "/movie",
   },
 };
