91755fa0b247b867591129b990029b5b33978f9e
Yotars <yot417@126.com>
2024-02-04 15:41:58 +0800

    完善左侧菜单

--- Frontend/LiYuanMovies/src/views/console/ConsoleView.vue
+++ Frontend/LiYuanMovies/src/views/console/ConsoleView.vue
@@ -1,24 +1,50 @@
 <template>
   <div class="container">
-    <n-layout :inverted="inverted" content-style="height: 100vh;">
-      <n-layout-header bordered>颐和园路</n-layout-header>
-      <n-layout has-sider>
-        <n-layout-sider bordered content-style="padding: 24px;">
-          <div style="height: 100%;">123</div>
-        </n-layout-sider>
-        <n-layout-content bordered content-style="padding: 24px;">
-          平山道
-        </n-layout-content>
-      </n-layout>
-      <n-layout-footer>成府路</n-layout-footer>
-    </n-layout>
+    <el-container>
+      <el-header style="padding: 0">
+        <div class="title-header container">
+          <h1 class="title" :val="text">{{ text }}</h1>
+        </div>
+      </el-header>
+      <el-container>
+        <el-aside>
+          <el-menu
+            active-text-color="#ffd04b"
+            background-color="#545c64"
+            class="menu"
+            :default-active="_active"
+            text-color="#fff"
+            router
+          >
+            <el-menu-item v-for="(i, key) in menu" :key="key" :index="i.path">
+              {{ i.name }}
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+        <el-main>
+          <router-view />
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 
-const inverted = ref(true);
+const text = ref("欢迎使用，梨园影城有后台管理系统");
+const rag = /(\S*)5173/;
+const href = window.location.href;
+const _active = href.replace(rag, "");
+const menu = [
+  { name: "首页", path: "/console/home" },
+  { name: "用户管理", path: "/console/user" },
+  { name: "电影管理", path: "/console/movie" },
+  { name: "影厅管理", path: "/console/room" },
+  { name: "订单管理", path: "/console/order" },
+];
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+@import url("../../style/console.css");
+</style>
