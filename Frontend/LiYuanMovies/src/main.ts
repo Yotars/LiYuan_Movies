d757a037459f55c51fd2335d65a6209c5ce0c061
Yotars <yot417@126.com>
2024-02-08 11:29:41 +0800

    添加汉化

--- Frontend/LiYuanMovies/src/main.ts
+++ Frontend/LiYuanMovies/src/main.ts
@@ -4,7 +4,11 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import { router } from "./router";
 
-import "@/style/common.css"
+import "@/style/common.css";
+
+import ElementPlus from "element-plus";
+
+import zhCn from "element-plus/es/locale/lang/zh-cn.mjs";
 
 const app = createApp(App);
 
@@ -12,3 +16,7 @@ app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
+
+app.use(ElementPlus, {
+  locale: zhCn,
+});
